881cead53a46579bedcf71364cec0366820fe77e
Thiago Medeiros <si-thiago@hotmail.com>
2020-07-17 19:24:45 -0300

    Report page components skeleton

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ import {AppRegistry} from 'react-native';
 import Main from './src/pages/Main';
 import {name as appName} from './app.json';
 import NewEntry from './src/pages/NewEntry';
+import Report from './src/pages/Report';
 
-AppRegistry.registerComponent(appName, () => NewEntry);
+AppRegistry.registerComponent(appName, () => Report);
