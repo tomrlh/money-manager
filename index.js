bc68f896b778cb6190d2077fc96594f8fe6fdfdd
Thiago Medeiros <si-thiago@hotmail.com>
2020-07-17 18:22:11 -0300

    NewEntry page (components skeleton)

--- index.js
+++ index.js
@@ -5,5 +5,6 @@
 import {AppRegistry} from 'react-native';
 import Main from './src/pages/Main';
 import {name as appName} from './app.json';
+import NewEntry from './src/pages/NewEntry';
 
-AppRegistry.registerComponent(appName, () => Main);
+AppRegistry.registerComponent(appName, () => NewEntry);
---
+++ src/pages/NewEntry/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import {Button, View, TextInput, StyleSheet} from 'react-native';
+
+import BalanceLabel from '../../components/BalanceLabel';
+
+const NewEntry = () => {
+  return (
+    <View style={styles.container}>
+      <BalanceLabel />
+
+      <View>
+        <TextInput style={styles.input} />
+        <TextInput style={styles.input} />
+        <Button title="GPS" />
+        <Button title="Camera" />
+      </View>
+
+      <View>
+        <Button title="Add" />
+        <Button title="Cancel" />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 10,
+  },
+  input: {
+    borderColor: '#000',
+    borderWidth: 1,
+  },
+});
+
+export default NewEntry;
